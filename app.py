23c9d783b9abf355534a2b82e0276a700db3465f
TomaszSzyborski <tomasz.szyborski@gmail.com>
2024-11-17 12:57:54 +0100

    added port by env variable

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 import sentry_sdk
 import uvicorn
@@ -57,4 +58,5 @@ app.add_middleware(
 app.include_router(api_router)
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=os.getenv("port", 80))
+    # uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
